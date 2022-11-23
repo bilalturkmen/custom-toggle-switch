a28fad7da495491c11c72815ebf22c33d6a9e5f1
bilalturkmen <turkmenbilal@gmail.com>
2022-11-24 00:59:21 +0300

    label text change when checkbox checked

--- app.js
+++ app.js
@@ -8,5 +8,19 @@ document.querySelectorAll('[role="switch"]').forEach((toggle) => {
     thumb.style.animation = isChecked
       ? "toggleOn 200ms cubic-bezier(0.4, 0, 0.6, 1) forwards"
       : "toggleOff 200ms cubic-bezier(0.4, 0, 0.6, 1) forwards";
+
+    const labelText = toggle
+      .closest(".custom-check-wrapper")
+      .querySelector("label");
+
+    const d = labelText.htmlFor;
+
+    if (isChecked) {
+      document.getElementById(d + "-checked").style.display = "inline";
+      document.getElementById(d + "-unchecked").style.display = "none";
+    } else {
+      document.getElementById(d + "-checked").style.display = "none";
+      document.getElementById(d + "-unchecked").style.display = "inline";
+    }
   });
 });
